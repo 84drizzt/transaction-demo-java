30b554b5ccfae62af7a8912c783a22e5626a89f9
84drizzt <84drizzt@gmail.com>
2025-04-21 15:42:59 +0800

    Init entities for JPA

---
+++ src/main/java/com/demo/transaction/TransactionDemoApplication.java
@@ -0,0 +1,11 @@
+package com.demo.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionDemoApplication.class, args);
+    }
+} 
---
+++ src/main/java/com/demo/transaction/enumeration/TransactionType.java
@@ -0,0 +1,7 @@
+package com.demo.transaction.enumeration;
+
+public enum TransactionType {
+    DEPOSIT,    // 存款
+    WITHDRAW,   // 取款
+    TRANSFER    // 转账
+} 
